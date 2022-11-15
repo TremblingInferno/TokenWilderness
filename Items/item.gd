6eccf9baf3c10922c363d78e4cbc39d9d19fb0b1
Jaime Kurzweg <jkurzweg10@gmail.com>
2022-11-15 12:32:13 -0700

    items + collecting

---
+++ Items/item.gd
@@ -0,0 +1,23 @@
+extends Sprite
+class_name Item
+
+export var item_name:String
+export(NodePath) onready var area2d = get_node(area2d) as Area2D
+export(Curve) var attractor_curve
+
+func _process(delta):
+	for area in area2d.get_overlapping_areas():
+		if area.is_in_group("Player"):
+			attract(area, delta)
+
+func attract(area, delta):
+	var diff = area.global_position - self.global_position
+	if diff.length() < 0.1:
+		pick_up(area)
+		return
+	var move_vector = lerp(self.global_position, area.global_position, attractor_curve.interpolate(1/diff.length()))
+	self.position = move_vector
+
+func pick_up(area):
+	queue_free()
+	GameEvents.emit_signal("item_picked_up", self)
