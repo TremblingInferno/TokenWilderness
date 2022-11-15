8ed398f5c8cdd9b751f7a974eaa8a840b96f4986
Jaime Kurzweg <jkurzweg10@gmail.com>
2022-11-15 12:33:12 -0700

    cleaned item

--- Items/item.gd
+++ Items/item.gd
@@ -10,14 +10,14 @@ func _process(delta):
 		if area.is_in_group("Player"):
 			attract(area, delta)
 
-func attract(area, delta):
+func attract(area, _delta):
 	var diff = area.global_position - self.global_position
 	if diff.length() < 0.1:
-		pick_up(area)
+		pick_up()
 		return
 	var move_vector = lerp(self.global_position, area.global_position, attractor_curve.interpolate(1/diff.length()))
 	self.position = move_vector
 
-func pick_up(area):
+func pick_up():
 	queue_free()
 	GameEvents.emit_signal("item_picked_up", self)
