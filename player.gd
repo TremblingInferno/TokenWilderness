660ca67713c13c9f05718771fb3f2c4aca09d4f5
Jaime Kurzweg <jkurzweg10@gmail.com>
2022-11-14 21:02:04 -0700

    workable movement

--- player.gd
+++ player.gd
@@ -1,26 +1,23 @@
 extends AnimatedSprite
 
-var velocity:Vector2 = Vector2(0,0)
+var velocity:Vector2 = Vector2(0,0) setget set_velocity
 export(int) var speed = 200
+
+var vertical_movement
+var horizontal_movement
+
+export(Resource) var right
+export(Resource) var left
+export(Resource) var up
+export(Resource) var down
 
 func _process(delta):
 	self.translate(speed * velocity.normalized() * delta)
+	handle_input()
 
-func _unhandled_key_input(event):
-	if event.is_action("up"):
-		up()
-	if event.is_action("down"):
-		down()
-	if event.is_action("right"):
-		right()
-	if event.is_action("left"):
-		left()
+func set_velocity(val):
+	velocity = val
 
-func up():
-	velocity.y = -1
-func down():
-	velocity.y = 1
-func right():
-	velocity.x = 1
-func left():
-	velocity.x = -1
+func handle_input():
+	set_velocity(Input.get_vector("left", "right", "up", "down"))
+
