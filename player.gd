2173a3d9e29e7bce7ee32e5d40ec9649c89cb853
Jaime Kurzweg <jkurzweg10@gmail.com>
2022-11-14 21:34:37 -0700

    removed setget velocity

--- player.gd
+++ player.gd
@@ -1,6 +1,6 @@
 extends AnimatedSprite
 
-var velocity:Vector2 = Vector2(0,0) setget set_velocity
+var velocity:Vector2 = Vector2(0,0)
 export(int) var speed = 200
 
 var vertical_movement
@@ -10,8 +10,6 @@ func _process(delta):
 	self.translate(speed * velocity.normalized() * delta)
 	handle_input()
 
-func set_velocity(val):
-	velocity = val
 
 func handle_input():
 	if Input.is_action_just_pressed("up") or Input.is_action_just_released("down"):
