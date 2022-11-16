cde0f136d50ac78538e4e7c72879819fefe97adf
Jaime Kurzweg <jkurzweg10@gmail.com>
2022-11-15 18:12:38 -0700

    fixed max_item bug

--- Inventory/Inventory.gd
+++ Inventory/Inventory.gd
@@ -3,10 +3,11 @@ extends HBoxContainer
 export(String) onready var inventory_slot = load(inventory_slot) as PackedScene
 export var max_items = 4
 
-var inventory:Array = []
+var item_number = 0
 
 func _ready():
 	GameEvents.connect("item_picked_up", self, "add_item_slot")
+	GameEvents.connect("dropped_item", self, "remove_item")
 	create_empty_inventory()
 
 
@@ -19,13 +20,21 @@ func create_empty_inventory():
 func add_item_slot(item):
 	var slot = get_next_open_slot()
 	if slot:
+		item_number += 1
 		slot.set_item(item)
+		if self.is_full():
+			GameEvents.emit_signal("inventory_full")
 
 func get_next_open_slot():
-	var slots = get_children()
-	for i in max_items:
-		if i == max_items-1:
-			GameEvents.emit_signal("inventory_full")
-		if !slots[i].is_full():
-			return slots[i]
-	return 0
+	for slot in get_children():
+		if not slot.is_full():
+			return slot
+
+func is_full():
+	if item_number >= max_items:
+		return true
+	return false
+
+func remove_item(_item):
+	if _item:
+		item_number -= 1
