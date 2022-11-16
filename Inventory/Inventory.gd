b32aa97f7cf67532233c8664dcf94a3a1ff20be3
Jaime Kurzweg <jkurzweg10@gmail.com>
2022-11-15 18:33:36 -0700

    added /n

--- Inventory/Inventory.gd
+++ Inventory/Inventory.gd
@@ -25,15 +25,18 @@ func add_item_slot(item):
 		if self.is_full():
 			GameEvents.emit_signal("inventory_full")
 
+
 func get_next_open_slot():
 	for slot in get_children():
 		if not slot.is_full():
 			return slot
 
+
 func is_full():
 	if item_number >= max_items:
 		return true
 	return false
+
 
 func remove_item(_item):
 	if _item:
