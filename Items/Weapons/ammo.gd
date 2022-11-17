84b1d3f900e602b67ffbc71354402450e2318812
Jaime Kurzweg <jkurzweg10@gmail.com>
2022-11-17 00:00:07 -0700

    spear speed

--- Items/Weapons/ammo.gd
+++ Items/Weapons/ammo.gd
@@ -2,6 +2,7 @@ extends Sprite
 
 var to_position:Vector2
 var from_position:Vector2
+var velocity:Vector2
 
 var weapon_resource:Resource
 var ammo_resource:Resource
@@ -12,15 +13,15 @@ func init_ammo(from, to, resource):
 	from_position = from
 	weapon_resource = resource
 	ammo_resource = weapon_resource.ammo
+
+	velocity = ammo_resource.speed * (to_position - from_position).normalized()
 	self.texture = ammo_resource.texture
 
-var change = 0
 
 func _process(delta):
-	if change >= 1:
+	if velocity.dot(to_position - position) <= 0:
 		drop_ammo()
-	change += ammo_resource.speed * delta
-	self.position = lerp(from_position, to_position, change)
+	self.translate(velocity * delta)
 
 
 func drop_ammo():
